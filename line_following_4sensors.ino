37602aa8de4f4d13ab1ca7fe641ff14b2464763c
nfcc2 <nfcc2@cam.ac.uk>
2023-01-25 09:25:03 +0000

    compares new and old sensor readings

--- line_following_4sensors.ino
+++ line_following_4sensors.ino
@@ -1,4 +1,7 @@
+// basic line following program
+
 // include required libraries
+#include <string>
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
@@ -9,7 +12,6 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *rightMotor = AFMS.getMotor(1); // pin M1
 Adafruit_DCMotor *leftMotor = AFMS.getMotor(2); //pin M2
 
-
 // optoswitch pins for line following
 #define farLeftOSwitch 6
 #define leftOSwitch 7
@@ -18,6 +20,7 @@ Adafruit_DCMotor *leftMotor = AFMS.getMotor(2); //pin M2
 
 // variables
 int motorSpeed = 150; // speed ranges from 0 to 255
+std::string sensorReading = "1001"; // string holding 4 optoswitch sensor readings
 
 void setup() {
   // put your setup code here, to run once:
@@ -32,22 +35,39 @@ void loop() {
 // functions go here
 
 void follow_line() {
-  // get optoswitch sensor readings from digital pins
-  bool farLeftV = digitalRead(farLeftOSwitch);
-  bool leftV = digitalRead(leftOSwitch);
-  bool rightV = digitalRead(rightOSwitch);
-  bool farRightV = digitalRead(farRightOSwitch);
+  // get optoswitch sensor readings from digital pins; 1=white, 0=black
+  bool V1 = digitalRead(farLeftOSwitch);
+  bool V2 = digitalRead(leftOSwitch);
+  bool V3 = digitalRead(rightOSwitch);
+  bool V4 = digitalRead(farRightOSwitch);
 
-  if (leftV == 0 && rightV == 0) { // both black
-    forward();
-  } else if (leftV == 1 && rightV == 0) {
-    turnLeft();
-  } else if (leftV == 0 && rightV == 1) {
-    turnRight();
-  } else if (leftV == 1 && rightV == 1) {
-    stop();
+  // convert 4 booleans to a string
+  std::string newSensorReading = to_string(V1) + to_string(V2) + to_string(V3) + to_string(V4)
+  
+  // if new readings are the same as the old readings, don't send repeated commands to the motors
+  if newSensorReading == sensorReading {
+    return
   }
-  delay(50); // frequency of sensor readings?
+
+  if sensorReading == "0110" {
+    forward();
+  } else if (sensorReading == "0100") {
+    turnLeft();
+  } else if (sensorReadinng == "0010") {
+    turnRight();
+  } else if (sensorReading == "1111") {
+    stop();
+  } else if sensorReading == "0000" {
+    // tunnel
+    forward();
+    Serial.print('Tunnel')
+  } else if sensorReading = "0111" {
+    // look for block and pick it up
+    Serial.print('Block')
+  } else if sensorReading = "1110" {
+    // decide whether to enter delivery box
+    Serial.print('Delivery')
+  }
 }
 
 // functions for movement
