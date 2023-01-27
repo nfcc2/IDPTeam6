a682f4e54d61a29766c138c8acdb4ca6734cddbc
nfcc2 <nfcc2@cam.ac.uk>
2023-01-27 16:39:12 +0000

    efs

--- main.ino
+++ main.ino
@@ -19,7 +19,7 @@ Adafruit_DCMotor *leftMotor = AFMS.getMotor(2); //pin M2
 // optoswitch pin for colour identification
 #define colourOSwitch 9
 
-// ultrasonic sensors
+// ultrasonic sensor pins sensor 1 front, sensor 2 left
 #define USonic1Trigger 10
 #define USonic1Echo 11
 #define USonic2Trigger 12
@@ -29,11 +29,12 @@ NewPing sonar1(USonic1Trigger, USonic1Echo, MAX_DISTANCE);
 NewPing sonar2(USonic2Trigger, USonic2Echo, MAX_DISTANCE);
 
 // variables
-int robotState = 0; // initial state
 int motorSpeed1 = 150; // speed ranges from 0 to 255
 int motorSpeed2 = -150;
-std::string sensorReading = "101"; // string holding 4 optoswitch sensor readings
-std::string distanceReading = "0"; //Inital distance reading provided there's no obstacle at the start
+
+// initialisation
+int robotState = 0;
+std::string sensorReading = "101"; 
 
 void setup() {
   // put your setup code here, to run once:
@@ -45,12 +46,18 @@ void loop() {
   // put your main code here, to run repeatedly:
 
   switch (robotState) {
-    case 0: // leave start area
-        Serial.println('State 1');
-        robotState = 2;
+    case 0: // leave start area and turn left
+        Serial.println('State 0');
+        forward();
+        delay(1000);
+        turnLeft();
+        delay(1000);
+        stop();
+        robotState = 1;
         break;
 
     case 1: // line following
+        Serial.println('State 1');
         robotState = 3;
         break;
 
@@ -64,53 +71,64 @@ void loop() {
 
 // functions go here
 
+// navigation functions
 void follow_line() {
-  // get optoswitch sensor readings from digital pins; 1=white, 0=black
-  bool V1 = digitalRead(farLeftOSwitch);
-  bool V2 = digitalRead(leftOSwitch);
-  bool V3 = digitalRead(rightOSwitch);
-  bool V4 = digitalRead(colourOSwitch);
-  bool V5 = digitalRead(USonic);
-
-  // convert 4 booleans to a string
-  std::string newSensorReading = to_string(V1) + to_string(V2) + to_string(V3);
-  std::string newDistanceReading = to_string(V5);
-
-  
+  newSensorReading = OSwitchReadings();
   // if new readings are the same as the old readings, don't send repeated commands to the motors
   if newSensorReading == sensorReading {
     return
   }
-  if distanceReading == newDistanceReading {
-    return
-  }
 
-  if newsensorReading == "000" {
-    forward();
-  } else if (newsensorReading == "010") {
-    turnLeft();
-  } else if (newsensorReadinng == "001") {
-    turnRight();
-  } else if (newsensorReading == "111") {
-    stop();
-  } else if newsensorReading == "110" {
-    // decide whether to enter delivery box
-    Serial.println('Delivery')
+  switch newSensorReading {
+    case "000":
+        forward();
+    case "010":
+        turnLeft();
+    case "001":
+        turnRight();
+    case "111":
+        stop();
+    case "110":
+        // detected left junction
   }
-
-  if newDistanceReading == "1" {
-    turnBack();
-  } else if (newsensorReading == "0100") {
-    return
 }
+
+// Check if robot is too close or too far away from the left wall
+void leftWallCheck() {           
+    if leftSensor() < 5 { // going towards left wall
+        turnBack(); // until we detect the lines again?
+    } else if leftSensor() > 30 {
+        // go towards wall
+    }
 }
 
 // sensor functions
 
-// This function returns the distance measured by the front USonic sensor. if robot gets too close to a wall, it turns back
+// Returns line following sensor readings in binary (left to right)
+void OSwitchReadings() {
+    bool V1 = digitalRead(farLeftOSwitch); // 1=white, 0=black
+    bool V2 = digitalRead(leftOSwitch);
+    bool V3 = digitalRead(rightOSwitch);
+    bool V4 = digitalRead(colourOSwitch);
+
+    // convert 4 booleans to a string
+    std::string newSensorReading = to_string(V1) + to_string(V2) + to_string(V3);
+    return newSensorReading;
+} 
+
+// These return the distance (in cm) measured by the front and left USonic sensors. if robot gets too close to a wall, it turns back
 void frontSensor() {
-    distance = (sonar.ping_median(5) / 2) * 0.0343; // averages 5 sensor readings
-    return distance;
+    distance = (sonar1.ping_median(5) / 2) * 0.0343; // averages 5 sensor readings
+    if distance != 0 { // remove anomalous zero readings
+        return distance;
+    }
+}
+
+void leftSensor() {
+    distance = (sonar1.ping_median(5) / 2) * 0.0343;
+    if distance != 0 { 
+        return distance;
+    }
 }
 
 // functions for movement
@@ -142,13 +160,13 @@ void turnRight() {
     leftMotor->run(FORWARD);
     rightMotor->run(RELEASE);
     delay(200);
+}
 
-void turnBack()
+void turnBack() {
    leftMotor.setSpeed(motorSpeed1);
-   rightMotor.setSpeed(moterSpeed2);
+   rightMotor.setSpeed(motorSpeed2);
    leftMotor->run(FORWARD);
    rightMotor->run(FORWARD);
-   delay(200)
-   
+   delay(200);
 }
 
