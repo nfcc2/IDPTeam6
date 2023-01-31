62ae1c315b531665b90fcf4307c30a8f797ff7c2
nfcc2 <nfcc2@cam.ac.uk>
2023-01-31 23:00:59 +0000

    bugs

---
+++ basic_main.ino
@@ -0,0 +1,246 @@
+// use line following sensors to go one loop clockwise around the arena. Pick up block if detected using push button.
+
+// include required libraries
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+#include "NewPing.h"
+
+// create objects and specify pins
+// motors
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Adafruit_DCMotor *rightMotor = AFMS.getMotor(1); // pin M1
+Adafruit_DCMotor *leftMotor = AFMS.getMotor(2); //pin M2
+
+// optoswitch pins for line following
+#define farLeftOSwitch 7
+#define leftOSwitch 6
+#define rightOSwitch 5
+
+// optoswitch pin for colour identification
+#define colourOSwitch 4
+
+// push button input pins
+const int blockButtonPin = 3;
+const int startButtonPin = 2;
+
+// LED output pins
+const int amberLEDPin = 
+
+// variable initialisation
+int robotState = 0;
+int motorSpeed1 = 150; // speed ranges from 0 to 255
+int count = 0; // counts number of left intersections
+bool holdingBlock = false;
+bool redBlock = false;
+bool start = false;
+String sensorReading = "010"; 
+
+void setup() {
+  AFMS.begin();
+  Serial.begin(9600);
+  pinMode(blockButtonPin, INPUT);
+  pinMode(startButtonPin, INPUT);
+}
+
+void loop() {
+  switch (robotState) {
+    case 0: // leave start area and turn left
+        Serial.println('State 0');
+        stop();
+
+        // robot starts moving when we push the start buttonn
+/*         while (start==false) {
+            if (digitalRead(startButtonPin) == true) {
+                start=true;
+                break;
+            }
+        } */
+        forward();
+        delay(1000);
+        if (OSwitchReadings() == "000") {
+            rotateLeft(90);
+        }
+        robotState = 1;
+        break;
+
+    case 1: // line following
+        follow_line();
+
+        // detect left intersection
+        if (OSwitchReadings() == "001") {
+            count++;
+
+            if (holdingBlock) {
+                if (redBlock && count%4 == 2) {
+                    robotState = 3;
+                    break;
+                } else if (!redBlock && count%4==0) {
+                    robotState = 3;
+                    break;
+                }
+            }
+        }
+
+        // detect block with push button
+        bool buttonState = digitalRead(blockButtonPin);
+        if (buttonState==1) {
+            robotState = 2;
+            break;
+        }
+
+        break;
+
+    case 2: // pick up block and identify colour
+        stop();
+        holdingBlock = true;
+
+        // activate gripper
+
+        delay(1000); // wait till gripper has finnished moving
+        redBlock = detectColour();
+
+        if (redBlock) {
+            Serial.print("Block colour is red");
+        } else {
+            Serial.print("Block colour is blue");
+        }
+        delay(500);
+
+        robotState = 1;
+        break;
+
+    case 3: // robot puts down block
+        // enter box
+        stop();
+        rotateLeft(90);
+        forward();
+        delay(1000);
+        stop();
+
+        // raise gripper and release block
+        holdingBlock = false;
+        delay(2000); // wait till gripper has finnished movinng
+
+        // reverse out
+        backward();
+        delay(1000);
+        rotateRight(90);
+        
+        robotState = 1;
+        break;
+  }
+}
+
+// functions go here
+
+// navigation functions
+void follow_line() {
+  String newSensorReading = OSwitchReadings();
+  // if new readings are the same as the old readings, don't send repeated commands to the motors
+  if (newSensorReading == sensorReading) {
+    return;
+  }
+  sensorReading = newSensorReading;
+
+  if (sensorReading == "111") {
+    forward();
+  } else if (sensorReading == "101") {
+    turnLeft();
+  } else if (sensorReading == "110") {
+    turnRight();
+  }
+}
+
+// sensor functions
+
+// Returns line following sensor readings in binary (left to right)
+String OSwitchReadings() {
+    bool V1 = digitalRead(farLeftOSwitch); // 1=black, 0=white
+    bool V2 = digitalRead(leftOSwitch);
+    bool V3 = digitalRead(rightOSwitch);
+
+    // convert 4 booleans to a string
+    String newSensorReading = String(V1) + String(V2) + String(V3);
+    return newSensorReading;
+} 
+
+// Returns boolean to indicate colour (Red=1, blue=0). Averages 5 sensor readings
+bool detectColour() {
+    int count = 0;
+    for (int i = 0; i < 5; i++) {
+        if (digitalRead(colourOSwitch) == 1) {
+            count++;
+        }
+        delay(100);
+    }
+    return (count >= 3);
+}
+
+// functions for movement
+void forward() {
+  leftMotor->setSpeed(motorSpeed1);
+  rightMotor->setSpeed(motorSpeed1);
+  leftMotor->run(FORWARD);
+  rightMotor->run(FORWARD);
+  delay(200);
+}
+
+void backward() {
+    leftMotor->setSpeed(motorSpeed1);
+  rightMotor->setSpeed(motorSpeed1);
+  leftMotor->run(BACKWARD);
+  rightMotor->run(BACKWARD);
+  delay(200);
+}
+
+void stop() {
+    leftMotor->run(RELEASE);
+    rightMotor->run(RELEASE);
+    delay(200);
+}
+
+void turnLeft() {
+    leftMotor->setSpeed(motorSpeed1);
+    rightMotor->setSpeed(motorSpeed1);
+    leftMotor->run(RELEASE);
+    rightMotor->run(FORWARD);
+    delay(200);
+}
+
+void turnRight() {
+    leftMotor->setSpeed(motorSpeed1);
+    rightMotor->setSpeed(motorSpeed1);
+    leftMotor->run(FORWARD);
+    rightMotor->run(RELEASE);
+    delay(200);
+}
+
+void turnBack() {
+   leftMotor->setSpeed(motorSpeed1);
+   rightMotor->setSpeed(motorSpeed1);
+   leftMotor->run(FORWARD);
+   rightMotor->run(BACKWARD);
+   delay(200);
+}
+
+void rotateLeft(int degrees) {
+    leftMotor->setSpeed(motorSpeed1);
+    rightMotor->setSpeed(motorSpeed1);
+    leftMotor->run(BACKWARD);
+    rightMotor->run(FORWARD);
+    delay((1000/90)*degrees); // 1000 is time taken to turn 90 degrees (assume 1 second for now)
+    leftMotor->run(RELEASE);
+    rightMotor->run(RELEASE);
+}
+
+void rotateRight(int degrees) {
+    leftMotor->setSpeed(motorSpeed1);
+    rightMotor->setSpeed(motorSpeed1);
+    leftMotor->run(FORWARD);
+    rightMotor->run(BACKWARD);
+    delay((1000/90)*degrees); // 1000 is time taken to turn 90 degrees (assume 1 second for now)
+    leftMotor->run(RELEASE);
+    rightMotor->run(RELEASE);
+}
+
--- tests/line_following_4sensors/line_following_final.ino
+++ tests/line_following_4sensors/line_following_final.ino
@@ -23,16 +23,6 @@ Adafruit_DCMotor *leftMotor = AFMS.getMotor(2); //pin M2
 // push button input pin
 const int buttonPin = 5;
 
-/* // ultrasonic sensor pins sensor 1 front, sensor 2 left
-#define USonic1Trigger 10
-#define USonic1Echo 11
-#define USonic2Trigger 12
-#define USonic2Echo 13
-#define MAX_DISTANCE 400
-NewPing sonar1(USonic1Trigger, USonic1Echo, MAX_DISTANCE);
-NewPing sonar2(USonic2Trigger, USonic2Echo, MAX_DISTANCE); */
-
-// variables
 int motorSpeed1 = 150; // speed ranges from 0 to 255
 int count = 0; // counts number of left intersections
 bool holdingBlock = false;
@@ -40,7 +30,7 @@ bool redBlock = false;
 
 // initialisation
 int robotState = 0;
-char sensorReading[] = "010"; 
+String sensorReading = "010";
 
 void setup() {
   // put your setup code here, to run once:
@@ -57,47 +47,53 @@ void loop() {
 
 // navigation functions
 void follow_line() {
-  newSensorReading = OSwitchReadings();
+  String newSensorReading = OSwitchReadings();
   // if new readings are the same as the old readings, don't send repeated commands to the motors
   if (newSensorReading == sensorReading) {
-    return
+    return;
   }
   sensorReading = newSensorReading;
 
-  switch newSensorReading { // 1=black, 0=white
-    case "111":
-        forward();
-    case "101":
-        turnLeft();
-    case "110":
-        turnRight();
-    case "000":
-        stop();
-    case "001":
-        stop();
+  if (sensorReading == "111") {
+    forward();
+  } else if (sensorReading == "101") {
+    turnLeft();
+  } else if (sensorReading == "110") {
+    turnRight();
+  } else if (sensorReading == "000") {
+    stop();
+  } else if (sensorReading == "001") {
+    stop();
   }
 }
 
 // sensor functions
 
 // Returns line following sensor readings in binary (left to right)
-void OSwitchReadings() {
+String OSwitchReadings() {
     bool V1 = digitalRead(farLeftOSwitch); // 1=black, 0=white
     bool V2 = digitalRead(leftOSwitch);
     bool V3 = digitalRead(rightOSwitch);
-    bool V4 = digitalRead(colourOSwitch);
 
     // convert 4 booleans to a string
-    char newSensorReading[] = to_string(V1) + to_string(V2) + to_string(V3);
+    String newSensorReading = String(V1) + String(V2) + String(V3);
     return newSensorReading;
 } 
 
 // functions for movement
 void forward() {
-  leftMotor.setSpeed(motorSpeed1);
-  rightMotor.setSpeed(motorSpeed1);
+  leftMotor->setSpeed(motorSpeed1);
+  rightMotor->setSpeed(motorSpeed1);
   leftMotor->run(FORWARD);
   rightMotor->run(FORWARD);
+  delay(200);
+}
+
+void backward() {
+    leftMotor->setSpeed(motorSpeed1);
+  rightMotor->setSpeed(motorSpeed1);
+  leftMotor->run(BACKWARD);
+  rightMotor->run(BACKWARD);
   delay(200);
 }
 
@@ -108,35 +104,35 @@ void stop() {
 }
 
 void turnLeft() {
-    leftMotor.setSpeed(motorSpeed1);
-    rightMotor.setSpeed(motorSpeed1);
+    leftMotor->setSpeed(motorSpeed1);
+    rightMotor->setSpeed(motorSpeed1);
     leftMotor->run(RELEASE);
     rightMotor->run(FORWARD);
     delay(200);
 }
 
 void turnRight() {
-    leftMotor.setSpeed(motorSpeed1);
-    rightMotor.setSpeed(motorSpeed1);
+    leftMotor->setSpeed(motorSpeed1);
+    rightMotor->setSpeed(motorSpeed1);
     leftMotor->run(FORWARD);
     rightMotor->run(RELEASE);
     delay(200);
 }
 
 void turnBack() {
-   leftMotor.setSpeed(motorSpeed1);
-   rightMotor.setSpeed(motorSpeed1);
+   leftMotor->setSpeed(motorSpeed1);
+   rightMotor->setSpeed(motorSpeed1);
    leftMotor->run(FORWARD);
    rightMotor->run(BACKWARD);
    delay(200);
 }
 
 void rotateLeft(int degrees) {
-    leftMotor.setSpeed(motorSpeed1);
-    rightMotor.setSpeed(motorSpeed1);
+    leftMotor->setSpeed(motorSpeed1);
+    rightMotor->setSpeed(motorSpeed1);
     leftMotor->run(BACKWARD);
     rightMotor->run(FORWARD);
-    delay((1000/90)*degrees) // 1000 is time taken to turn 90 degrees (assume 1 second for now)
+    delay((1000/90)*degrees); // 1000 is time taken to turn 90 degrees (assume 1 second for now)
     leftMotor->run(RELEASE);
     rightMotor->run(RELEASE);
 }
