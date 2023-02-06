f850ab8d0f1442cbc0df96116d2f03faeb3d051a
nfcc2 <nfcc2@cam.ac.uk>
2023-02-06 17:58:48 +0000

    Added functions to test

---
+++ tests/motor_test/motor_test.ino
@@ -0,0 +1,176 @@
+// use line following sensors to go one loop clockwise around the arena. Pick up block if detected using push button.
+
+// include required libraries
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+#include "NewPing.h"
+#include <Arduino.h>
+
+// create objects and specify pins
+// motors
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Adafruit_DCMotor *rightMotor = AFMS.getMotor(1); // pin M1
+Adafruit_DCMotor *leftMotor = AFMS.getMotor(2); //pin M2
+
+// push button input pins
+const int startButtonPin = 2;
+const int blockButtonPin = 3;
+
+// optoswitch pins for line following
+#define farLeftOSwitch 4
+#define leftOSwitch 5
+#define rightOSwitch 6
+#define farRightOSwitch 7
+
+// LED output pins
+const int amberLEDPin = 8;
+const int redLEDPin = 10; // nneed to write code to activate these pins after identifying colour
+const int greenLEDPin = 11;
+
+// optoswitch pin for colour identification
+#define colourOSwitch A2
+
+// variable initialisation
+int robotState = 0;
+const int motorSpeed1 = 255; // speed ranges from 0 to 255
+const int rotateTime = 1550; // time taken to rotate 90 degrees
+int count = 0; // counts number of left intersections
+bool holdingBlock = false;
+bool redBlock = false;
+bool start = false;
+String sensorReading = "0101"; 
+// for flashing amber LED when moving
+bool LEDState = false; 
+bool startLED = false;
+
+
+void setup() {
+  //Serial.begin(9600);
+  // put your setup code here, to run once:
+  AFMS.begin();
+  Serial.begin(9600);
+    //pinMode(blockButtonPin, INPUT);
+  //pinMode(startButtonPin, INPUT);
+  pinMode(amberLEDPin, OUTPUT);
+  
+  // set up timer interrupt for flashing amber LED
+  cli();  //stop interrupts for till we make the settings
+  TCB0.CTRLB = TCB_CNTMODE_INT_gc; // Use timer compare mode
+  TCB0.CCMP = 125000; // Value to compare with. 25000 is 1/10th of the tick rate, so 10 Hz
+  TCB0.INTCTRL = TCB_CAPT_bm; // Enable the interrupt
+  TCB0.CTRLA = TCB_CLKSEL_CLKTCA_gc | TCB_ENABLE_bm; // Use Timer A as clock, enable timer
+  sei(); //Enable back the interrupts
+stop();
+  Serial.println(millis());
+//rotateLeft(90);
+Serial.println(millis());
+//forward();
+}
+
+void loop() {
+  forward();
+  delay(4000);
+  backward();
+  delay(4000);
+  turnLeft();
+  delay(4000);
+  turnRight();
+  delay(4000);
+  backLeft();
+  delay(4000);
+  backRight();
+  delay(4000);
+}
+
+
+// functions for movement
+void forward() {
+  leftMotor->setSpeed(motorSpeed1);
+  rightMotor->setSpeed(motorSpeed1);
+  leftMotor->run(FORWARD);
+  rightMotor->run(FORWARD);
+  startLED = true;
+}
+
+void backward() {
+  leftMotor->setSpeed(motorSpeed1);
+  rightMotor->setSpeed(motorSpeed1);
+  leftMotor->run(BACKWARD);
+  rightMotor->run(BACKWARD);
+  startLED = true;
+}
+
+void stop() {
+    leftMotor->run(RELEASE);
+    rightMotor->run(RELEASE);
+    startLED = false;
+}
+
+void turnLeft() {
+    leftMotor->setSpeed(motorSpeed1);
+    rightMotor->setSpeed(motorSpeed1);
+    leftMotor->run(RELEASE);
+    rightMotor->run(FORWARD);
+    startLED = true;
+}
+
+void turnRight() {
+    leftMotor->setSpeed(motorSpeed1);
+    rightMotor->setSpeed(motorSpeed1);
+    leftMotor->run(FORWARD);
+    rightMotor->run(RELEASE);
+    startLED = true;
+}
+
+void backLeft() {
+    leftMotor->setSpeed(motorSpeed1);
+    rightMotor->setSpeed(motorSpeed1*0.75);
+    leftMotor->run(BACKWARD);
+    rightMotor->run(BACKWARD);
+    startLED = true;
+}
+
+void backRight() {
+    leftMotor->setSpeed(motorSpeed1*0.75);
+    rightMotor->setSpeed(motorSpeed1);
+    leftMotor->run(BACKWARD);
+    rightMotor->run(BACKWARD);
+    startLED = true;
+}
+
+void rotateLeft(int degrees) {
+    leftMotor->setSpeed(motorSpeed1);
+    rightMotor->setSpeed(motorSpeed1);
+    startLED = true;
+    leftMotor->run(BACKWARD);
+    rightMotor->run(FORWARD);
+    delay((rotateTime/90)*degrees); // 1000 is time taken to turn 90 degrees (assume 1 second for now)
+    leftMotor->run(RELEASE);
+    rightMotor->run(RELEASE);
+}
+
+void rotateRight(int degrees) {
+    leftMotor->setSpeed(motorSpeed1);
+    rightMotor->setSpeed(motorSpeed1);
+    startLED = true;
+    leftMotor->run(FORWARD);
+    rightMotor->run(BACKWARD);
+    delay((rotateTime/90)*degrees); // 1000 is time taken to turn 90 degrees (assume 1 second for now)
+    leftMotor->run(RELEASE);
+    rightMotor->run(RELEASE);
+}
+
+// LED functions
+
+// Flashing amber LED. Triggered every 500ms (2 Hz)
+ISR(TCB0_INT_vect) {
+if (startLED) { // startLED = true when motors are running
+  LEDState = !LEDState; // toggle LED bbetween on and off
+  } else {
+    LEDState = 0; // turn off LED
+  }
+digitalWrite(amberLEDPin, LEDState);
+// Clear interrupt flag
+TCB0.INTFLAGS = TCB_CAPT_bm;
+}
